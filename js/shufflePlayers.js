a50c5971e16cb82e4ce4d42ecf4e13202fb224b6
kaihavlik <kai@havlik.ch>
2020-05-24 00:58:06 +0200

    created javascript to shuffle players

--- js/shufflePlayers.js
+++ js/shufflePlayers.js
@@ -1,24 +1,41 @@
 const shuffleButton = document.querySelector('#btn-shuffle')
 
 shuffleButton.addEventListener('click', () => {
-  shuffle()
+  shufflePlayers()
 })
 
-function shuffle() {
-  if (activePlayers == 0) index = document.querySelectorAll('.rows.quarter input')
-  if (activePlayers == 1) index = document.querySelectorAll('.rows.eight input')
-
-  const allPlayers = index
-
-  /*let namesArray = Array.from(allPlayers)  
-
-  for (let i = namesArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [namesArray[i], namesArray[j]] = [namesArray[j], namesArray[i]];
-  }
+function shufflePlayers() {
+  // check how many players are playing (defined in players.js)
+  if (activePlayers == 0) allPlayers = document.querySelectorAll('.rows.quarter input')
+  if (activePlayers == 1) allPlayers = document.querySelectorAll('.rows.eight input')
+  // create array from inputs
+  var allPlayersArr = [];
 
   for (let i = 0; i < allPlayers.length; i++) {
-    allPlayers[i].value = namesArray[i].value
-    console.log(namesArray[i].value);
-  }*/
+    allPlayersArr.push(allPlayers[i].value);
+  }
+  // shuffle array
+  shuffleArr(allPlayersArr)
+  // append shuffled players
+  let index = 0
+  for (let i = 0; i < allPlayers.length; i++) {
+    allPlayers[i].value = allPlayersArr[index]
+    index++
+  }
+}
+
+function shuffleArr(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // while there remain elements to shuffle
+  while (0 !== currentIndex) {
+    // pick a remaining element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // swap it with the current element
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
 }
