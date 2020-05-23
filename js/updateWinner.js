300a4adae1fadce40ca98ab47d9e245ea014a879
kaihavlik <kai@havlik.ch>
2020-05-23 02:00:18 +0200

    small improvements

--- js/updateWinner.js
+++ js/updateWinner.js
@@ -1,7 +1,12 @@
 const nextBtn = document.querySelectorAll('.textbox i')
 
+function updateWinner() {
+console.log('test');
+
+}
+
 for (let i = 0; i < nextBtn.length; i++) {
   nextBtn[i].addEventListener('click', () => {
-    
+    updateWinner()
   })  
 }
