2fc8672db18e90e189d65fb7e013bd8c96ede013
kaihavlik <kai@havlik.ch>
2020-05-24 02:39:18 +0200

    shuffle animation

--- js/shufflePlayers.js
+++ js/shufflePlayers.js
@@ -1,28 +1,51 @@
 const shuffleButton = document.querySelector('#btn-shuffle')
 
-shuffleButton.addEventListener('click', () => {
-  shufflePlayers()
+// check how many players are playing (defined in players.js)
+if (activePlayers == 0) allPlayers = document.querySelectorAll('.rows.quarter input')
+if (activePlayers == 1) allPlayers = document.querySelectorAll('.rows.eight input')
+
+// gsap timeline ------------------------------------------
+let tlShuffle = new gsap.timeline({
+  paused: true,
+  defaults: {
+    ease: Power1.easeInOut
+  }
 })
 
-function shufflePlayers() {
-  // check how many players are playing (defined in players.js)
-  if (activePlayers == 0) allPlayers = document.querySelectorAll('.rows.quarter input')
-  if (activePlayers == 1) allPlayers = document.querySelectorAll('.rows.eight input')
-  // create array from inputs
-  var allPlayersArr = [];
+tlShuffle
+.fromTo(allPlayers, {opacity: 1}, {opacity: 0})
 
+.add( function () {
+  let index = 0
+
+  for (let i = 0; i < allPlayers.length; i++) {
+    allPlayers[i].value = allPlayersArr[index]
+    index++
+  }
+})
+
+.set(allPlayers, {opacity: 0, scale: 2,})
+
+.to(allPlayers, {
+  opacity: 1,
+  stagger: 0.5,
+  scale: 1,
+})
+
+// shuffle funtion ------------------------------------------
+
+shuffleButton.addEventListener('click', () => {
+  // create array from inputs
+  allPlayersArr = [];
   for (let i = 0; i < allPlayers.length; i++) {
     allPlayersArr.push(allPlayers[i].value);
   }
   // shuffle array
   shuffleArr(allPlayersArr)
   // append shuffled players
-  let index = 0
-  for (let i = 0; i < allPlayers.length; i++) {
-    allPlayers[i].value = allPlayersArr[index]
-    index++
-  }
-}
+  tlShuffle.play(0)
+  console.log(allPlayersArr);
+})
 
 function shuffleArr(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
