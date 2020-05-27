4ea19dbd4fa57d35b92c868697044a10a9a1f60d
kaihavlik <kai@havlik.ch>
2020-05-28 00:30:18 +0200

    temporary and bad updateWinner.js

--- js/autoscalefontsize.js
+++ js/autoscalefontsize.js
@@ -3,12 +3,14 @@
   const allInputs = document.querySelectorAll('#rounds-container input')
 
   for (let i = 0; i < allInputs.length; i++) {
+    // Run function on start (use only if there player placeholders)
     // Set max lenght
     allInputs[i].maxLength = 12
     // Set default font-size
     allInputs[i].style.fontSize = '11pt'
     // Trigger function on input change
     allInputs[i].addEventListener("input", (e) => scaleFontSize(e.target))
+    scaleFontSize(allInputs[i])
   }
 
   // Trigger function on windows change
@@ -23,16 +25,16 @@
 function scaleFontSize(input) {    
   // If text is overflowing
   if (input.scrollHeight > input.clientHeight || input.scrollWidth > input.clientWidth){
-    // Font-size -2
+    // Font-size -1
     input.style.fontSize = parseInt(input.style.fontSize) - 1 + 'pt'  
     // Repeat
     scaleFontSize(input)
   } else {
-    // Font-size +2
+    // Font-size +1
     input.style.fontSize = parseInt(input.style.fontSize) + 1 + 'pt'
     // If text is overflowing or max font-size
     if (input.scrollHeight > input.clientHeight || input.scrollWidth > input.clientWidth || input.style.fontSize == '13pt') {
-      // Font-size -1
+      // Font-size -2
       input.style.fontSize = parseInt(input.style.fontSize) - 2 + 'pt'
     } else {scaleFontSize(input)}
   }
--- js/updateWinner.js
+++ js/updateWinner.js
@@ -1,12 +1,53 @@
-const nextBtn = document.querySelectorAll('.textbox i')
+const nextBtn8 = document.querySelectorAll('.rounds.eighth .textbox i')
+const nextBtn4 = document.querySelectorAll('.rounds.quarter .textbox i')
+const nextBtn2 = document.querySelectorAll('.rounds.semi .textbox i')
+const nextBtn1 = document.querySelectorAll('.rounds.final .textbox i')
 
-function updateWinner() {
-console.log('test');
+function updateWinner(i, j) {
+  console.group()
+  console.log('PlayerNo: ' + (i+1));
 
+  let round = j.parentNode.parentNode
+  if (round.classList.contains('textbox-container')) round = j.parentNode.parentNode.parentNode
+
+  let winner = j.parentNode.querySelector('input').value
+  console.log('Playername: ' + winner);
+
+  if (round.classList.contains('eighth')) {
+    newRound = document.querySelectorAll('.rounds.quarter input')
+    newRound[Math.floor(i/2)].value = winner
+  }
+  if (round.classList.contains('quarter')) {
+    newRound = document.querySelectorAll('.rounds.semi input')
+    newRound[Math.floor(i/2)].value = winner
+  }
+  if (round.classList.contains('semi')) {
+    newRound = document.querySelectorAll('.rounds.final input')
+    newRound[Math.floor(i/2)].value = winner
+  }
+  if (round.classList.contains('final')) {
+  }
+
+  console.groupEnd()
 }
 
-for (let i = 0; i < nextBtn.length; i++) {
-  nextBtn[i].addEventListener('click', () => {
-    updateWinner()
+for (let i = 0; i < nextBtn8.length; i++) {
+  nextBtn8[i].addEventListener('click', (e) => {
+    updateWinner(i, e.target)
+  })  
+}
+for (let i = 0; i < nextBtn4.length; i++) {
+  nextBtn4[i].addEventListener('click', (e) => {
+    updateWinner(i, e.target)
+  })  
+}
+for (let i = 0; i < nextBtn2.length; i++) {
+  nextBtn2[i].addEventListener('click', (e) => {
+    updateWinner(i, e.target)
+  })  
+}
+for (let i = 0; i < nextBtn1.length; i++) {
+  nextBtn1[i].addEventListener('click', (e) => {
+    updateWinner(i, e.target)
   })  
 }
