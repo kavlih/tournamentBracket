a79502afea9603f1ca59e19f18546a34e9f982d8
kaihavlik <kai@havlik.ch>
2020-05-27 03:39:07 +0200

    improved font-size

--- js/autoscalefontsize.js
+++ js/autoscalefontsize.js
@@ -6,7 +6,7 @@
     // Set max lenght
     allInputs[i].maxLength = 12
     // Set default font-size
-    allInputs[i].style.fontSize = '12pt'
+    allInputs[i].style.fontSize = '11pt'
     // Trigger function on input change
     allInputs[i].addEventListener("input", (e) => scaleFontSize(e.target))
   }
@@ -23,17 +23,17 @@
 function scaleFontSize(input) {    
   // If text is overflowing
   if (input.scrollHeight > input.clientHeight || input.scrollWidth > input.clientWidth){
-    // Font-size -1
+    // Font-size -2
     input.style.fontSize = parseInt(input.style.fontSize) - 1 + 'pt'  
     // Repeat
     scaleFontSize(input)
   } else {
-    // Font-size +1
+    // Font-size +2
     input.style.fontSize = parseInt(input.style.fontSize) + 1 + 'pt'
     // If text is overflowing or max font-size
     if (input.scrollHeight > input.clientHeight || input.scrollWidth > input.clientWidth || input.style.fontSize == '13pt') {
       // Font-size -1
-      input.style.fontSize = parseInt(input.style.fontSize) - 1 + 'pt'
+      input.style.fontSize = parseInt(input.style.fontSize) - 2 + 'pt'
     } else {scaleFontSize(input)}
   }
   console.log(input.style.fontSize);
