8ea9816f96e087ce0c7c8bfa9b5ccdf62498d181
kaihavlik <kai@havlik.ch>
2020-06-04 20:43:32 +0200

    fixed a bug where you couldn't shuffle players if '8 players' was selected

--- js/shufflePlayers.js
+++ js/shufflePlayers.js
@@ -1,36 +1,36 @@
 (function(){
-  // Check how many players are playing (defined in players.js)
-  if (amountOfPlayers == 0) allPlayers = document.querySelectorAll('.rounds.quarter .player')
-  if (amountOfPlayers == 1) allPlayers = document.querySelectorAll('.rounds.eighth .player')
-
-  // GSAP Timeline - Shuffle ------------------------------------------
-  const tlShuffle = new gsap.timeline({paused: true, defaults: {ease: Power1.easeInOut}})
-
-  tlShuffle
-  .fromTo(allPlayers, {opacity: 1}, {opacity: 0})
-  
-  .add(function () {
-    let index = 0
-
-    for (let i = 0; i < allPlayers.length; i++) {
-      // Append shuffled players
-      allPlayers[i].value = allPlayersArr[index]
-      // Check font size
-      scaleFontSize(allPlayers[i])
-      index++
-    }
-  })
-
-  .set(allPlayers, {opacity: 0, scale: 2,})
-
-  .to(allPlayers, {
-    opacity: 1,
-    stagger: 0.5,
-    scale: 1,
-  })
-
   // Event listener - Click on shuffle button ------------------------------------------
   document.querySelector('#btn-shuffle').addEventListener('click', () => {
+    // Check how many players are playing (defined in players.js)
+    if (amountOfPlayers == 0) allPlayers = document.querySelectorAll('.rounds.quarter .player')
+    if (amountOfPlayers == 1) allPlayers = document.querySelectorAll('.rounds.eighth .player')    
+
+    // GSAP Timeline - Shuffle ------------------------------------------
+    const tlShuffle = new gsap.timeline({paused: true, defaults: {ease: Power1.easeInOut}})
+
+    tlShuffle
+    .fromTo(allPlayers, {opacity: 1}, {opacity: 0})
+    
+    .add(function () {
+      let index = 0
+
+      for (let i = 0; i < allPlayers.length; i++) {
+        // Append shuffled players
+        allPlayers[i].value = allPlayersArr[index]
+        // Check font size
+        scaleFontSize(allPlayers[i])
+        index++
+      }
+    })
+
+    .set(allPlayers, {opacity: 0, scale: 2,})
+
+    .to(allPlayers, {
+      opacity: 1,
+      stagger: 0.5,
+      scale: 1,
+    })
+
     if (!tlShuffle.isActive()) {
       // Create array from inputs
       allPlayersArr = [];
